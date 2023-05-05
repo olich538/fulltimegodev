16e56f07bb292c28a873773fb51906010215d698
Ol <olegpolezhai@MacBook-Pro.fios-router.home>
2023-05-05 15:25:17 -0500

    initial commit

---
+++ hotel-reservation/api/user_handler.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/olich538/fulltimegodev/hotel-reservation/hotel-reservation/db"
+	"github.com/olich538/fulltimegodev/hotel-reservation/hotel-reservation/types"
+)
+
+type UserHandlerStore struct {
+	userStore db.UserStore
+}
+
+// constructor function
+func NewUserHandler(userStore db.UserStore) *UserHandlerStore {
+	return &UserHandlerStore{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandlerStore) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id = c.Params("id")
+	)
+	user, err := h.userStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+func (h *UserHandlerStore) HandleGetUsers(c *fiber.Ctx) error {
+	// u := types.User{
+	// 	FirstName: "James",
+	// 	LastName:  "WaterKooler",
+	// }
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(&users)
+}
+
+func (h *UserHandlerStore) HandlePostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	user, err := types.NewUserFromParams(params)
+	if err != nil {
+		return err
+	}
+
+	insertedUser, err := h.userStore.InsertUser(c.Context(), user)
+	if err != nil {
+		return err
+	}
+	return c.JSON(insertedUser)
+}
---
+++ hotel-reservation/db/user_store.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"context"
+
+	"github.com/olich538/fulltimegodev/hotel-reservation/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const userColl = "users"
+
+type PostgresUserStore struct{}
+
+type UserStore interface {
+	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUser(context.Context, *types.User) (*types.User, error)
+}
+
+type MongoUserStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
+	return &MongoUserStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection(userColl),
+	}
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	var users []*types.User
+	// TODO implement query filter
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	if err := cur.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
